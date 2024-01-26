5599916af9822e8e62a20c386a5b31c6e2078180
GustavoUNAL <garteagae@unal.edu.co>
2024-01-26 09:47:24 -0500

    Añadir archivo .gitignore para ignorar ciertas carpetas

--- nasa_flow.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import json
-
-# Abrir el archivo JSON y cargar los datos
-with open('DataLake/NASA/POWER_Point_Daily_20230101_20240101_001d23N_077d29W_LST.json', 'r') as f:
-    data = json.load(f)
-    print(data["properties"])
