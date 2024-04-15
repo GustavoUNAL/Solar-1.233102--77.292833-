5dfdbfa7b1099ccd9e06e2d2affc407ab34f3ad9
GustavoUNAL <garteagae@unal.edu.co>
2024-04-15 17:25:42 -0500

    new tensors with all 2023

--- Models/main.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
-
-# Cargar el conjunto de datos MNIST
-(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
